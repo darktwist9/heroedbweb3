61f42e2c1a897916cedba75b394ffb8805f34d0f
Hamza Cassis <hamza.cassis@gmail.com>
2026-09-18 08:24:30 -0400

    añadiendo Identity

--- Program.cs
+++ Program.cs
@@ -1,14 +1,72 @@
 using Microsoft.EntityFrameworkCore;
 using HeroesWeb.Data;
+using Microsoft.AspNetCore.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+
+{
+
+    options.Conventions.AuthorizeFolder("/Heroes");
+
+    options.Conventions.AuthorizeFolder("/SuperPoderes");
+
+});
 
 builder.Services.AddDbContext<HeroesContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("HeroesDb")
         ?? throw new InvalidOperationException("Falta la conexión HeroesDb.")));
+
+var connectionString =
+
+builder.Configuration.GetConnectionString("HeroesDb")
+
+?? throw new InvalidOperationException(
+
+"No se encontró la conexión HeroesDb.");
+
+builder.Services.AddRazorPages();
+
+
+
+builder.Services.AddDbContext<HeroesContext>(options =>
+
+options.UseSqlServer(connectionString));
+
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
+options.UseSqlServer(connectionString));
+
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+
+{
+
+    options.SignIn.RequireConfirmedAccount = false;
+
+
+    options.User.RequireUniqueEmail = true;
+
+
+    options.Password.RequiredLength = 8;
+
+    options.Password.RequireDigit = true;
+
+    options.Password.RequireLowercase = true;
+
+    options.Password.RequireUppercase = true;
+
+    options.Password.RequireNonAlphanumeric = true;
+
+})
+
+.AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -20,6 +78,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages().WithStaticAssets();
